5c6d5bf1109405fa225414f6a79ada6cc3524374
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-09-18 15:34:29 +0530

    test

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#@app.route("/")
+#def landing():
+#   return render_template("index.html")
+
 @app.route("/")
 def landing():
-    return render_template("index.html")
+    return "Hello, World!  This is the landing page to let everyone know that the pratham is gay."
 
 if __name__ == "__main__":
     app.run(debug=True)
